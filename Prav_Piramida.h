4a887fb1e564205566b87d678d7613c783bc3641
Ντμίτρι <dimitri.alexandr5w@yandex.ru>
2024-03-16 19:13:38 +0300

    Update Prav_Piramida.h

--- Prav_Piramida.h
+++ Prav_Piramida.h
@@ -1,11 +1,46 @@
 #include "Piramida.h"
-
-class Prav_Piramida: public Piramida {
-public:
-	void changing_edges(int);
-	void changing_base_sides(int, int);
-	double calculating_surface_area();
-	void changing_base_sides_value(int);
-	double calculating_side_surface_area();
+class Prav_Piramida:public Piramida
+{
 private:
+    double triangle_area(double a, double b, double c);
+public:
+    void set_ribs(double);
+    void set_bases(double);
+    double area();
+    double side_area();
+    void set(double a, double b);
 };
+
+double Prav_Piramida::triangle_area(double a, double b, double c) {
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+void Prav_Piramida::set(double a, double b) {
+    rib1 = a;
+    rib2 = a;
+    rib3 = a;
+    base1 = b;
+    base2 = b;
+    base3 = b;
+}
+
+void Prav_Piramida::set_ribs(double a) {
+    rib1 = a;
+    rib2 = a;
+    rib3 = a;
+}
+
+void Prav_Piramida::set_bases(double b) {
+    base1 = b;
+    base2 = b;
+    base3 = b;
+}
+
+double Prav_Piramida::area() { 
+    return triangle_area(base1, base2, base3) + triangle_area(base1, rib1, rib2) * 3;
+}
+
+double Prav_Piramida::side_area() {
+    return triangle_area(base1, rib1, rib2) * 3;
+}
