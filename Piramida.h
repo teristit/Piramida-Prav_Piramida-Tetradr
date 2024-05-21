d0273583239eba9581a74e72b005f20e1ded2a2b
idcanyways <159810995+idcanyways@users.noreply.github.com>
2024-05-21 18:47:37 +0300

    Update Piramida.h
    добавил условия существования

--- Piramida.h
+++ Piramida.h
@@ -1,12 +1,13 @@
 #pragma once
 #include <iostream>
 namespace figure {
+    template <typename T>
     class Piramida {
     protected:
-        double rib1, rib2, rib3, base1, base2, base3;
+        T rib1, rib2, rib3, base1, base2, base3;
     public:
         Piramida() {}
-        Piramida(double a, double b, double c, double d, double e, double f)
+        Piramida(T a, T b, T c, T d, T e, T f)
             :rib1(a), rib2(b), rib3(c), base1(d), base2(e), base3(f) {}
         ~Piramida() {
             std::cout << "Deleted";
@@ -14,15 +15,19 @@ namespace figure {
         void show() {
             std::cout << rib1 << " " << rib2 << " " << rib3 << " " << base1 << " " << base2 << " " << base3 << " \n";
         }
-        virtual void set(double a, double b, double c, double d, double e, double f) {
-            rib1 = a;
-            rib2 = b;
-            rib3 = c;
-            base1 = d;
-            base2 = e;
-            base3 = f;
+        virtual void set(T a, T b, T c, T d, T e, T f) {
+            if ((!triaglearea(d, e, f) + !triaglearea(f, a, b) + !triaglearea(e, a, c) + !triaglearea(d, b, c)) == 0) {
+                rib1 = a;
+                rib2 = b;
+                rib3 = c;
+                base1 = d;
+                base2 = e;
+                base3 = f;
+            }
+            else
+                std::cout << "Неправильный ввод";
         }
-        virtual void get(double& a, double& b, double& c, double& d, double& e, double& f) {
+        virtual void get(T& a, T& b, T& c, T& d, T& e, T& f) {
             a = rib1;
             b = rib2;
             c = rib3;
@@ -30,30 +35,52 @@ namespace figure {
             e = base2;
             f = base3;
         }
-        virtual void setr1(double a) {
+        virtual void setr1(T a) {
+            if (!triaglearea(base3, a, rib2) + !triaglearea(base2, a, rib3) == 0)
             rib1 = a;
+            else 
+                std::cout << "Неправильный ввод";
         }
-        virtual void setr2(double a) {
-            rib2 = a;
+        virtual void setr2(T a) {
+            if (!triaglearea(base2, rib1, a) + !triaglearea(base1, a, rib3) == 0)
+                rib2 = a;
+            else
+                std::cout << "Неправильный ввод";
         }
-        virtual void setr3(double a) {
-            rib3 = a;
+        virtual void setr3(T a) {
+            if (!triaglearea(base3, rib1, a) + !triaglearea(base1, rib2, a) == 0)
+                rib3 = a;
+            else
+                std::cout << "Неправильный ввод";
         }
-        virtual void setb1(double a) {
-            base1 = a;
+        virtual void setb1(T a) {
+            if (!triaglearea(base2, base3, a) + !triaglearea(rib3, rib2, a) == 0)
+                base1 = a;
+            else
+                std::cout << "Неправильный ввод";
         }
-        virtual void setb2(double a) {
-            base2 = a;
+        virtual void setb2(T a) {
+            if (!triaglearea(rip1, rip3, a) + !triaglearea(base1, base3, a) == 0)
+                base2 = a;
+            else
+                std::cout << "Неправильный ввод";
         }
-        virtual void setb3(double a) {
-            base3 = a;
+        virtual void setb3(T a) {
+            if (!triaglearea(base2, base1, a) + !triaglearea(rib1, rib2, a) == 0)
+                base3 = a;
+            else
+                std::cout << "Неправильный ввод";
         }
 
-        static double triaglearea(double a, double b, double c) {
-            double p = (a + b + c) / 2;
-            return sqrt(abs(p * (p - a) * (p - b) * (p - c)));
+        double triaglearea(T a, T b, T c) {
+            if (a + b > c && a + c > b && b + c > a) {
+                T p = (a + b + c) / 2;
+                return sqrt(abs(p * (p - a) * (p - b) * (p - c)));
+            }
+            else
+                return 0;
         }
-        virtual double area() {
+        virtual T area() {
             return triaglearea(base1, base2, base3) + triaglearea(base3, rib1, rib2) + triaglearea(base2, rib1, rib3) + triaglearea(base1, rib2, rib3);
         }
     };
