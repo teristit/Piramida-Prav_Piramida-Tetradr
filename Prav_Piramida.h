41eb05fcfd93a671862905a37f1337d2daa294ab
idcanyways <159810995+idcanyways@users.noreply.github.com>
2024-05-21 18:49:00 +0300

    Update Prav_Piramida.h
    добавил template

--- Prav_Piramida.h
+++ Prav_Piramida.h
@@ -2,49 +2,42 @@
 #include "Piramida.h"
 
 using namespace figure;
-
-class Prav_Piramida:public Piramida
+template <typename T>
+class Prav_Piramida :public Piramida <T>
 {
 private:
-    double triangle_area(double a, double b, double c);
+    T triangle_area(T a, T b, T c) {
+        T p = (a + b + c) / 2;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+    }
 public:
-    void set_ribs(double);
-    void set_bases(double);
-    double area();
-    double side_area();
-    void set(double a, double b);
+
+    void set(T a, T b) {
+        rib1 = a;
+        rib2 = a;
+        rib3 = a;
+        base1 = b;
+        base2 = b;
+        base3 = b;
+    }
+
+    void set_ribs(T a) {
+        rib1 = a;
+        rib2 = a;
+        rib3 = a;
+    }
+
+    void set_bases(T b) {
+        base1 = b;
+        base2 = b;
+        base3 = b;
+    }
+
+    T area() {
+        return triangle_area(base1, base2, base3) + triangle_area(base1, rib1, rib2) * 3;
+    }
+
+    T side_area() {
+        return triangle_area(base1, rib1, rib2) * 3;
+    }
 };
-
-double Prav_Piramida::triangle_area(double a, double b, double c) {
-    double p = (a + b + c) / 2;
-    return sqrt(p * (p - a) * (p - b) * (p - c));
-}
-
-void Prav_Piramida::set(double a, double b) {
-    rib1 = a;
-    rib2 = a;
-    rib3 = a;
-    base1 = b;
-    base2 = b;
-    base3 = b;
-}
-
-void Prav_Piramida::set_ribs(double a) {
-    rib1 = a;
-    rib2 = a;
-    rib3 = a;
-}
-
-void Prav_Piramida::set_bases(double b) {
-    base1 = b;
-    base2 = b;
-    base3 = b;
-}
-
-double Prav_Piramida::area() { 
-    return triangle_area(base1, base2, base3) + triangle_area(base1, rib1, rib2) * 3;
-}
-
-double Prav_Piramida::side_area() {
-    return triangle_area(base1, rib1, rib2) * 3;
-}
