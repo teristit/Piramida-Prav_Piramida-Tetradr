efb683238d93110fcc9e9e7b60ca15c38b48dace
idcanyways <159810995+idcanyways@users.noreply.github.com>
2024-05-21 10:14:55 +0300

    Update main.cpp
    заебался уже

--- main.cpp
+++ main.cpp
@@ -2,13 +2,12 @@
 #include <Tetradr.h>
 #include <Prav_Piramida.h>
 #include <iostream>
-using namespace figure;
 /// Kurshin
 void piramidamenu() {
 	Piramida object;
 	int userchoise = -1;
-	std::cout << "Здравствйте, выберите действие:\n" << "0: Выход из программы\n" << "1: Ввести стороны пирамиды\n" << "2: Ввести ребро\n" 
-		<< "3: Ввести основание\n" << "4: Вывести обьём пирамиды \n" << "5: Вывести пирамиду\n";
+	std::cout << "Здравствйте, выберите действие:\n" << "0: Выход из программы\n" << "1: Ввести стороны пирамиды\n" << "2: Ввести ребро\n"
+		<< "3: Ввести основание\n" << "4: Вывести площадь пирамиды \n" << "5: Вывести пирамиду\n";
 	while (std::cin >> userchoise) {
 		switch (userchoise)
 		{
@@ -18,31 +17,29 @@ void piramidamenu() {
 			std::cout << "Введите строны пирамиды (3 ребра, 3 основания)\n";
 			double r1, r2, r3, b1, b2, b3;
 			std::cin >> r1 >> r2 >> r3 >> b1 >> b2 >> b3;
-			object.set(r1,r2,r3,b1,b2,b3);
+			object.set(r1, r2, r3, b1, b2, b3);
 			break;
 		case 2:
 			int temp;
+			double tempo;
 			std::cout << "Введите номер ребра для ввода (1-3)\n";
 			std::cin >> temp;
 			switch (temp)
 			{
 			case 1:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setr1(temp);
+				std::cin >> tempo;
+				object.setr1(tempo);
 				break;
 			case 2:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setr2(temp);
+				std::cin >> tempo;
+				object.setr2(tempo);
 				break;
 			case 3:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setr3(temp);
+				std::cin >> tempo;
+				object.setr3(tempo);
 				break;
 			default:
 				std::cout << "Нет такого ребра\n";
@@ -51,36 +48,35 @@ void piramidamenu() {
 			break;
 		case 3: {
 			int temp;
+			double tempo;
 			std::cout << "Выберите основание для ввода (1-3)\n";
 			std::cin >> temp;
 			switch (temp)
 			{
 			case 1:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setb1(temp);
+
+				std::cin >> tempo;
+				object.setb1(tempo);
 				break;
 			case 2:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setb2(temp);
+				std::cin >> tempo;
+				object.setb2(tempo);
 				break;
 			case 3:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setb3(temp);
+				std::cin >> tempo;
+				object.setb3(tempo);
 				break;
 			default:
 				std::cout << "Нет такого основания\n";
-					break;
+				break;
 			}
 		}
 			  break;
-		case 4: 
-			std::cout<<object.area();
+		case 4:
+			std::cout << object.area();
 			break;
 		case 5:
 			object.show();
@@ -95,11 +91,12 @@ void tetradrmenu() {
 	Tetradr object;
 	int userchoise = -1;
 	std::cout << "Здравствйте, выберите действие:\n" << "0: Выход из программы\n" << "1: Ввести стороны тетраедра\n" << "2: Ввести ребро\n"
-		<< "3: Ввести основание\n" << "4: Вывести площадь пирамиды \n" << "5: Вывести обьём пирамиды\n" << "6: Вывести боковую поверность\n";
+		<< "3: Ввести стороны тетраедра\n" << "4: Вывести площадь тетраедра \n" << "5: Вывести обьём тетраедра\n" << "6: Вывести боковую поверность\n";
 	while (std::cin >> userchoise) {
 		switch (userchoise)
 		{
 		case 0:
+			std::cout << "Выход в главное меню";
 			return;
 		case 1:
 			std::cout << "Введите строны пирамиды (3 ребра, 3 основания)\n";
@@ -109,27 +106,25 @@ void tetradrmenu() {
 			break;
 		case 2:
 			int temp;
+			double tempo;
 			std::cout << "Введите номер ребра для ввода (1-3)\n";
 			std::cin >> temp;
 			switch (temp)
 			{
 			case 1:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setr1(temp);
+				std::cin >> tempo;
+				object.setr1(tempo);
 				break;
 			case 2:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setr2(temp);
+				std::cin >> tempo;
+				object.setr2(tempo);
 				break;
 			case 3:
 				std::cout << "Введите длину\n";
-				double temp;
-				std::cin >> temp;
-				object.setr3(temp);
+				std::cin >> tempo;
+				object.setr3(tempo);
 				break;
 			default:
 				std::cout << "Нет такого ребра\n";
@@ -142,13 +137,13 @@ void tetradrmenu() {
 		}
 			  break;
 		case 4:
-			std::cout << object.area();
+			std::cout << object.area() <<"\n";
 			break;
 		case 5:
-			std::cout << object.volume();
+			std::cout << object.volume() << "\n";
 			break;
 		case 6:
-			std::cout << object.side_surface();
+			std::cout << object.side_surface() << "\n";
 			break;
 		default:
 			std::cout << "Нет такого действия\n";
@@ -181,5 +176,6 @@ void mainmenu() {
 }
 
 int main() {
+	setlocale(LC_ALL, "Russian");
 	mainmenu();
 }
